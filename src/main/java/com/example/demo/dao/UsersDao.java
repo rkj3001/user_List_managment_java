10ea745e0aa3fa51a0988aca3f7ed00cb78ef7b7
rkj3001 <rk.j3001@gmail.com>
2024-09-03 13:01:20 +0100

    fix: some space

--- src/main/java/com/example/demo/dao/UsersDao.java
+++ src/main/java/com/example/demo/dao/UsersDao.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+
+
+
+
 @Repository
 public interface UsersDao extends JpaRepository<User, Integer> {
 //no methods here
